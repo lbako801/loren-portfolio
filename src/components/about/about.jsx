8332c2acafe63102dfec1709f8db8f90957d84c8
lbako801 <lbako98@gmail.com>
2023-02-21 18:05:26 -0700

    changed casing issue for cloning!

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <h2>Who am I?</h2>
       
       <div className='about-content'>
-        <img src={PIC} className='image-1' />
+        <img src={PIC} className='image-1' alt= 'Loren in ASCII'/>
         
         <div className='about-info-container'>
         <p className='about-info'><span className='line-break'>>></span> I'm a full-stack engineer who combines tech skills with UI/UX design. I'm based in SLC and have worked on web dev projects and helped local businesses boost their online presence.<br></br><br></br><span className='line-break'>>></span> My front-end expertise includes HTML, CSS, JS, and React. I'm also skilled in backend and database programming with Node.js, MySQL, MongoDB, and PostgreSQL.<br></br><br></br><span className='line-break'>>></span> In my free time, I'm a vintage tech fan, motorcycle lover, and a dungeon master for D&D. I bring a fun and personable approach to work and love connecting with the tech community.</p>
--- src/components/work/work.jsx
+++ src/components/work/work.jsx
@@ -1,29 +1,117 @@
-import React from 'react'
-import './work.css'
-import TDBSS from '../../assets/images/Date-A-Base.png'
+import React from "react";
+import "./work.css";
+import TDBSS from "../../assets/images/Date-A-Base.png";
 
 const Work = () => {
   return (
-    <section id='work' className='work-section'>
+    <section id="work" className="work-section">
       <h2>Here is some recent work</h2>
 
-      <div className='work-card-container'>
-
-        <div className='work-card'>
-          <h3>THE DATE-A-BASE</h3>
-          <img src={TDBSS} />
-          <p>Web app using HTML, CSS, and JavaScript to provide couples with meal, drink, and movie
-            recommendations, complete with detailed information and pictures
-          </p>
-          <div className='work-btns'>
-            <a href='https://github.com/lbako801/The-Date-a-base/tree/develop' target='_blank' rel='noreferrer' className='btn'>CODE</a>
-            <a href='https://lbako801.github.io/The-Date-a-base/' target='_blank' rel='noreferrer' className='btn'>DEMO</a>
+      <div className="work-card-container">
+        <div className="work-card">
+          <h3>COMING SOON</h3>
+          <img src={TDBSS} alt="" />
+          <p>under construction....</p>
+          <div className="work-btns">
+            <a
+              href="https://github.com/lbako801"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              CODE
+            </a>
+            {/* <a href='https://lbako801.github.io/The-Date-a-base/' target='_blank' rel='noreferrer' className='btn'>DEMO</a> */}
           </div>
         </div>
 
+        <div className="work-card">
+          <h3>COMING SOON</h3>
+          <img src={TDBSS} alt="" />
+          <p>under construction....</p>
+          <div className="work-btns">
+            <a
+              href="https://github.com/lbako801"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              CODE
+            </a>
+            {/* <a href='https://lbako801.github.io/The-Date-a-base/' target='_blank' rel='noreferrer' className='btn'>DEMO</a> */}
+          </div>
+        </div>
+
+        <div className="work-card">
+          <h3>COMING SOON</h3>
+          <img src={TDBSS} alt="" />
+          <p>under construction....</p>
+          <div className="work-btns">
+            <a
+              href="https://github.com/lbako801"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              CODE
+            </a>
+            {/* <a href='https://lbako801.github.io/The-Date-a-base/' target='_blank' rel='noreferrer' className='btn'>DEMO</a> */}
+          </div>
+        </div>
+
+        <div className="work-card">
+          <h3>COMING SOON</h3>
+          <img src={TDBSS} alt="" />
+          <p>under construction....</p>
+          <div className="work-btns">
+            <a
+              href="https://github.com/lbako801"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              CODE
+            </a>
+            {/* <a href='https://lbako801.github.io/The-Date-a-base/' target='_blank' rel='noreferrer' className='btn'>DEMO</a> */}
+          </div>
+        </div>
+
+        <div className="work-card">
+          <h3>COMING SOON</h3>
+          <img src={TDBSS} alt="" />
+          <p>under construction....</p>
+          <div className="work-btns">
+            <a
+              href="https://github.com/lbako801"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              CODE
+            </a>
+            {/* <a href='https://lbako801.github.io/The-Date-a-base/' target='_blank' rel='noreferrer' className='btn'>DEMO</a> */}
+          </div>
+        </div>
+
+        <div className="work-card">
+          <h3>COMING SOON</h3>
+          <img src={TDBSS} alt="" />
+          <p>under construction....</p>
+          <div className="work-btns">
+            <a
+              href="https://github.com/lbako801"
+              target="_blank"
+              rel="noreferrer"
+              className="btn"
+            >
+              CODE
+            </a>
+            {/* <a href='https://lbako801.github.io/The-Date-a-base/' target='_blank' rel='noreferrer' className='btn'>DEMO</a> */}
+          </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
